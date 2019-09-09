d2dff1438b88f8fbb6113b0632fd7bb1976242c0
okan-s <okans@live.se>
2019-09-09 16:23:48 +0200

    Removed some unecessary stuff in Colors.js

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,5 @@
-const tintColor = '#2f95dc';
+const tintColor = '#B69F58';
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
 };
