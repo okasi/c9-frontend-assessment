bca5975b1dee8268b18b602a5a5d9a7dff9be02c
okan-s <okans@live.se>
2019-09-09 16:22:37 +0200

    More icons

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import React, { useState } from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, Feather, EvilIcons } from "@expo/vector-icons";
 
 import AppNavigator from "./navigation/AppNavigator";
 import NavigationService from "./utils/NavigationService";
@@ -18,22 +18,20 @@ export default function App(props) {
       return {
         width: "40%",
         margin: "0 auto",
-        position: 'fixed',
-        right: '0',
-        left: '0',
-        marginRight: 'auto',
-        marginLeft: 'auto',
-        backgroundColor: "#fff",
-      }
+        position: "fixed",
+        right: "0",
+        left: "0",
+        marginRight: "auto",
+        marginLeft: "auto",
+        backgroundColor: "#fff"
+      };
     } else {
       return {
         flex: 1,
-        backgroundColor: "#fff",
-      }
+        backgroundColor: "#fff"
+      };
     }
   }
-
-
 
   if (!isLoadingComplete && !props.skipLoadingScreen) {
     return (
@@ -45,12 +43,8 @@ export default function App(props) {
     );
   } else {
     return (
-      <View
-        style={{ width: '100%', flex: '1' }}
-      >
-        <View
-          style={getContainerStyle()}
-        >
+      <View style={{ flex: "1" }}>
+        <View style={getContainerStyle()}>
           {Platform.OS === "ios" && <StatusBar barStyle="default" />}
           <AppNavigator
             ref={navigatorRef => {
@@ -71,10 +65,10 @@ async function loadResourcesAsync() {
       require("./assets/images/icon.png")
     ]),
     Font.loadAsync({
-      // This is the font that we are using for our tab bar
       ...Ionicons.font,
-      // We include SpaceMono because we use it in StartScreen.js. Feel free to
-      // remove this if you are not using it in your app
+      ...Feather.font,
+      ...EvilIcons.font,
+
       "miller-banner": require("./assets/fonts/MillerBanner-Light.otf")
     })
   ]);
@@ -89,10 +83,3 @@ function handleLoadingError(error) {
 function handleFinishLoading(setLoadingComplete) {
   setLoadingComplete(true);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  }
-});
